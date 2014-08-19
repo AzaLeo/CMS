8b8f53928d3cd9087d47315e999031367bb7ab55
Leo <leo@iploz.com>
2014-08-19 16:22:33 +0300

    Добавлен файл readme.txt Небольшие исправления в коде

--- CMS/Site.Master.cs
+++ CMS/Site.Master.cs
@@ -9,7 +9,7 @@ namespace CMS
 {
     public partial class Site : System.Web.UI.MasterPage
     {
-        public const string HeadTitle = "Тестовый сайт для SSA";
+        public const string HeadTitle = "Тестовый сайт";
 
         protected void Page_Load(object sender, EventArgs e)
         {
